eb0893faa19ec2cb3e74e7d956a2a87a74eb3ab5
benggri <benggri@gmail.com>
2023-12-14 10:27:15 +0900

    update

--- .devcontainer/startup.sh
+++
@@ -1,7 +0,0 @@
-sed -i 's/codespaces/agnoster/' ~/.zshrc && \
-sed -i'' -r -e "/prompt_hg/a\  prompt_newline" ~/.oh-my-zsh/themes/agnoster.zsh-theme && \
-# cp .devcontainer/.ssh/id_rsa* ~/.ssh/ && \
-echo $(pwd) && \
-ssh -T git@github.com && \
-npm install && \
-echo DONE
