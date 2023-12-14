62b6ebbd2d3d39d4cceeb3dc24ab379539e067bb
benggri <benggri@gmail.com>
2023-12-14 10:03:19 +0900

    deploy to gh-pages

---
+++ out/_next/static/chunks/app/page-5c0f788564b1f9ef.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{1559:function(n,e,u){Promise.resolve().then(u.t.bind(u,1749,23))}},function(n){n.O(0,[749,971,938,744],function(){return n(n.s=1559)}),_N_E=n.O()}]);
