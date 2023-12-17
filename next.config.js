98aa903e0683eca38a6b7020d3a6c6bc877cd735
benggri <benggri@gmail.com>
2023-12-18 07:15:24 +0900

    update config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'docs',
+  // distDir: 'docs',
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
   reactStrictMode: true,
