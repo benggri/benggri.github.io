7340207e4bbdb4b3a010d21200df1c03cb454883
benggri <benggri@gmail.com>
2023-12-18 07:11:32 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  trailingSlash: true,  // 빌드 시 폴더 구조 그대로 생성하도록
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  distDir: 'docs',
+  trailingSlash: true,
   skipTrailingSlashRedirect: true,
   reactStrictMode: true,
   images: {
